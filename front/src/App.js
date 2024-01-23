a7696bdd65b2a82c70e00395e58296f0cfb233e0
4thdraw <4thdraw2@gmail.com>
2024-01-23 12:20:04 +0900

    리액트 데이터 노출하기

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [content, setcont] = useState("");
+  const [content, setcont] = useState([]);
   
   useEffect(
     ()=>{
@@ -11,17 +11,17 @@ function App() {
       // req.query -> 쿼리스트링 변수= 값 
       // naviapp.use(express.urlencoded({ extended : true }))
       // data 라우터 파일이 res.send 보내주는 데이터를
-      axios.get('http://localhost:8080/data?tablenm=qna')
+      axios.get('http://localhost:8080/data?tablenm=navi')
      .then((Response)=>{ // 여기 변수에 저장
-          console.log(typeof Response.data, Response.data);
-          setcont(Response.data);
+          console.log(typeof Response.data, Response.data, Response.data[0].members[0].name);
+          setcont([...Response.data[0].members]);
          })
      .catch((Error)=>{console.log(Error)})
     }, []
   )
   return (
     <div className="App">
-      {content}
+      {content.map((e, i)=> e.name)}
     </div>
   );
 }
--- server/routers/navi.js
+++ server/routers/navi.js
@@ -5,6 +5,46 @@ naviapp.use(express.json())
 // req.query변수가져오기 위해서 
 naviapp.use(express.urlencoded({ extended : true }))
 
+const navidata = [
+    {
+        "squadName": "Super hero squad",
+        "homeTown": "Metro City",
+        "formed": 2016,
+        "secretBase": "Super tower",
+        "active": true,
+        "members": [
+          {
+            "name": "Molecule Man",
+            "age": 29,
+            "secretIdentity": "Dan Jukes",
+            "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+          },
+          {
+            "name": "Madame Uppercut",
+            "age": 39,
+            "secretIdentity": "Jane Wilson",
+            "powers": [
+              "Million tonne punch",
+              "Damage resistance",
+              "Superhuman reflexes"
+            ]
+          },
+          {
+            "name": "Eternal Flame",
+            "age": 1000000,
+            "secretIdentity": "Unknown",
+            "powers": [
+              "Immortality",
+              "Heat Immunity",
+              "Inferno",
+              "Teleportation",
+              "Interdimensional travel"
+            ]
+          }
+        ]
+      }
+]
+
 naviapp.get('/',(req,res,next)=>{ 
 
       const info = req.query.tablenm;  // pc
@@ -12,9 +52,7 @@ naviapp.get('/',(req,res,next)=>{
       if(info == "navi"){ 
           //next('route'); 
           //다음라우터 실행
-          res.send(`${info}테이블 데이터 쿼리문 돌려서 보낸준 자료
-          http://localhost:8080/data?tablenm=navi
-          `) 
+          res.send(navidata) 
       }else if(info =="swiper"){
           res.send(`${info}테이블 http://localhost:8080/data?tablenm=swiper`)
       }else if(info =="qna"){
