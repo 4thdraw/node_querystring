ae347368a7af2bcc20f30392c0d3851553dcd208
4thdraw <4thdraw2@gmail.com>
2024-01-23 11:39:19 +0900

    연동처리해보기

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,19 @@
+import {useEffect} from 'react'
 import axios from 'axios';
 import './App.css';
 
 function App() {
   
+  useEffect(
+    ()=>{
+      axios.get('http://localhost:8080/data?tablenm=navi')
+     .then((Response)=>{
+          console.log(Response.data);
+
+         })
+     .catch((Error)=>{console.log(Error)})
+    }, []
+  )
   return (
     <div className="App">
      
