fb3595ef2ee9fbfcf118c1db9cdab8e13f924f0a
4thdraw <4thdraw2@gmail.com>
2024-01-24 12:56:40 +0900

    로컬에서 리액트와 노드서버 데이터교환하기

--- front/src/App.js
+++ front/src/App.js
@@ -4,24 +4,24 @@ import './App.css';
 
 function App() {
   const [content, setcont] = useState([]);
+
+  const tableselect = async (tablenm) =>{
+    axios.post(`http://localhost:8080/data?tablenm=${tablenm}`)
+    .then((Response)=>{ // 여기 변수에 저장
+         console.log(typeof Response.data, Response.data, Array.isArray(Response.data));
+         setcont([...Response.data]);
+        })
+    .catch((Error)=>{console.log(Error)})
+  }   
   
   useEffect(
-    ()=>{
-      // 서버요청 -> web.js가 /data 라우터넘겨주기 -> req 저장
-      // req.query -> 쿼리스트링 변수= 값 
-      // naviapp.use(express.urlencoded({ extended : true }))
-      // data 라우터 파일이 res.send 보내주는 데이터를
-      axios.get('http://localhost:8080/data?tablenm=navi')
-     .then((Response)=>{ // 여기 변수에 저장
-          console.log(typeof Response.data, Response.data, Response.data[0].members[0].name);
-          setcont([...Response.data[0].members]);
-         })
-     .catch((Error)=>{console.log(Error)})
+    ()=> { 
+      tableselect("swiper");
     }, []
   )
   return (
     <div className="App">
-      {content.map((e, i)=> e.name)}
+      { content.map((e, i) => e.src )}
     </div>
   );
 }
--- server/routers/navi.js
+++ server/routers/navi.js
@@ -1,67 +1,41 @@
 const express = require("express")
 const naviapp = express.Router();
-//중요 (라우터의 쿼리스트링을 일반문자가 아닌 데이터로 인식하게 하기 )
+
+//데이터전송
 naviapp.use(express.json())
-// req.query변수가져오기 위해서 
+//주소창 ? 뒤에부분을 key와 value인식
 naviapp.use(express.urlencoded({ extended : true }))
+// localhost:8080/data?tablenm=swiper
+const navidata = {
+  navinm : [
+    { nm : "회사소개", href : "#none"
+    },
+    { nm : "제품소개", href : "#none"
+    },
+    { nm : "견적서", href : "#none"
+    }
+  ] ,
+  swiper : [
+    { src : "/img/1.jpg", href : "#none"
+    },
+    { src : "/img/2.jpg", href : "#none"
+    },
+   { src : "/img/3.jpg", href : "#none"
+   }
+  ] 
+}
 
-const navidata = [
-    {
-        "squadName": "Super hero squad",
-        "homeTown": "Metro City",
-        "formed": 2016,
-        "secretBase": "Super tower",
-        "active": true,
-        "members": [
-          {
-            "name": "Molecule Man",
-            "age": 29,
-            "secretIdentity": "Dan Jukes",
-            "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
-          },
-          {
-            "name": "Madame Uppercut",
-            "age": 39,
-            "secretIdentity": "Jane Wilson",
-            "powers": [
-              "Million tonne punch",
-              "Damage resistance",
-              "Superhuman reflexes"
-            ]
-          },
-          {
-            "name": "Eternal Flame",
-            "age": 1000000,
-            "secretIdentity": "Unknown",
-            "powers": [
-              "Immortality",
-              "Heat Immunity",
-              "Inferno",
-              "Teleportation",
-              "Interdimensional travel"
-            ]
-          }
-        ]
-      }
-]
+naviapp.post('/',(req,res,next)=>{ 
+    const tablenm = req.query.tablenm;
+    if(tablenm == "navinm"){
+      res.send(navidata.navinm)
 
-naviapp.get('/',(req,res,next)=>{ 
-
-      const info = req.query.tablenm;  // pc
-      const no = req.query.w;  // 1   
-      if(info == "navi"){           
-          next('route'); 
-          //다음라우터 실행         
-      }else if(info =="swiper"){
-          res.send(`${info}테이블 http://localhost:8080/data?tablenm=swiper`)
-      }else if(info =="qna"){
-          res.send(`${info}테이블 노드자료를 리액트에게 전달`)
-      }else{
-         res.send("일반라우터")   
-      }
-})
-naviapp.get('/',(req,res,next)=>{ 
-    res.send({bbb:navidata}) 
+    }else if(tablenm == "swiper"){
+      res.send(navidata.swiper)
+    }else{
+      res.send(["일반라우터 ? 다음의 변수가없거나 설정안된 변수네"])
+    }
+     
 })
 
 module.exports = naviapp;
